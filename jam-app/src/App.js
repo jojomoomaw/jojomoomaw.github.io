580f4518f6797cc1ecce3cecc5d8d7c7c9ba6b86
jojomoomaw <joseph.moomaw@gmail.com>
2022-11-14 11:57:29 -0600

    personalizations for stage zero

--- jam-app/src/App.js
+++ jam-app/src/App.js
@@ -8,6 +8,12 @@ function App() {
           JAM APP STAGE ZERO
         </h1>
       </header>
+      <body className="App-body">
+        <h2>My React Projects</h2>
+          <h3>Empty... for now</h3>
+        <h2>LaunchCode LiftOff Capstone Project:</h2>
+          <h3><a href="https://github.com/Jul-2022-LC-LiftOff/Adventureland">Adventureland</a></h3>
+      </body>
     </div>
   );
 }
